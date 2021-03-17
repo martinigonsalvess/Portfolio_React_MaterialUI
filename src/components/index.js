4026fec520ebf58399ac5655697d65e392346079
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-03-17 16:26:14 +0100

    add in Particles

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,31 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Header from "./Header";
+import Particles from "react-particles-js";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  particlesCanva: {
+    position: "absolute",
+  },
+});
 
 const Home = () => {
+  const classes = useStyles();
   return (
     <div>
       <Navbar />
       <Header />
+      <Particles
+        canvasClassName={classes.particlesCanva}
+        params={{
+          particles: {
+            number: {
+              value: 45,
+            },
+          },
+        }}
+      />
     </div>
   );
 };
